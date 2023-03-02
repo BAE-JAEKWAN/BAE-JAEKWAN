b52c296bb24eb50c4ae5603d534c7f94d939b8a7
Bae JaeKwan <toxictears84@gmail.com>
2023-03-02 19:44:33 +0900

    리액트 hook에 취한다 : useEffect

--- 리액트 hook에 취한다/react-hook/src/App.js
+++ 리액트 hook에 취한다/react-hook/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Timer from "./component/Timer";
 
 const heavyWork = () => {
   console.log("엄청 무거운 작업");
@@ -35,28 +36,48 @@ function App() {
     });
   };
 
-  console.log("업데이트!!");
+  useEffect(() => {
+    console.log("useEffect : 랜더링 될 때 마다 실행");
+  }, [name]);
+
+  const [showTimer, setShowTimer] = useState(false);
 
   return (
     <div>
-      <span>현재 시간 {state}시</span>
-      <button type="button" onClick={handleClick}>
-        Update
-      </button>
-
-      <div>
-        {name.map((el, index) => {
-          return (
-            <p key={index}>
-              {index}. {el}
-            </p>
-          );
-        })}
-        <input type="text" value={input} onChange={handleInputChange} />
-        <button type="button" onClick={setNameFunc}>
+      <section>
+        <h1>useState, useEffect</h1>
+        <span>현재 시간 {state}시</span>
+        <button type="button" onClick={handleClick}>
           Update
         </button>
-      </div>
+
+        <div>
+          {name.map((el, index) => {
+            return (
+              <p key={index}>
+                {index}. {el}
+              </p>
+            );
+          })}
+          <input type="text" value={input} onChange={handleInputChange} />
+          <button type="button" onClick={setNameFunc}>
+            Update
+          </button>
+        </div>
+      </section>
+
+      <section>
+        <h1>useEffect</h1>
+        {showTimer && <Timer />}
+        <button
+          type="button"
+          onClick={() => {
+            setShowTimer(!showTimer);
+          }}
+        >
+          Toggle timer
+        </button>
+      </section>
     </div>
   );
 }
