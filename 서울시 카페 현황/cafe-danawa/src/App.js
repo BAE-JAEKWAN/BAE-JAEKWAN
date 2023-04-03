63f4236323cfc8ba1514bc6d108c4c25a6fee269
Bae JaeKwan <toxicviolet84@gamil.com>
2023-04-03 17:13:15 +0900

    현위치 주변 카페 현황 구현 - 속성값 수정

--- 서울시 카페 현황/cafe-danawa/src/App.js
+++ 서울시 카페 현황/cafe-danawa/src/App.js
@@ -11,14 +11,16 @@ function App() {
       <header>
         <h1>현위치 주변 카페 현황</h1>
       </header>
+
       <NavermapsProvider ncpClientId="p6cikodn76">
-        <MapDiv
-          style={{
-            height: 400,
-          }}
-        >
-          <NaverMap>
-            <Marker defaultPosition={{ lat: 37.5666103, lng: 126.9783882 }} />
+        <MapDiv style={{ width: "100%", height: "calc(100vh - 86px)" }}>
+          <NaverMap
+            defaultCenter={{ lat: 37.5666102, lng: 126.9783881 }} // 초기 중심 좌표
+            defaultZoom={13} // 초기 줌 레벨
+          >
+            <Marker
+              position={{ lat: 37.5666102, lng: 126.9783881 }} // 마커 좌표
+            />
           </NaverMap>
         </MapDiv>
       </NavermapsProvider>
