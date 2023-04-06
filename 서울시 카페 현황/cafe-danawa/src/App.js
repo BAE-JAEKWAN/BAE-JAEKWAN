f41ebb0aee2fb4957e735786b51524a00f1fdd69
Bae-JaeKwan <jaekwan@dfluid.kr>
2023-04-06 18:18:45 +0900

    현위치 주변 카페 현황 구현 - 카페 정보 API 연결 : 카카오 로컬 API - API에서 받아온 좌표 정보로 <Marker> 생성

--- 서울시 카페 현황/cafe-danawa/src/App.js
+++ 서울시 카페 현황/cafe-danawa/src/App.js
@@ -7,6 +7,7 @@ import {
   InfoWindow,
 } from "react-naver-maps";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const navermaps = useNavermaps();
@@ -15,40 +16,63 @@ function App() {
   const [localName, setLocalName] = useState(null); // 현재 위치 지역 이름
   const [localAddress, setLocalAddress] = useState(null); // 현재 위치 상세 주소
 
-  const [selectedMarker, setSelectedMarker] = useState(null); // 카페 상세 정보창
+  const [cafeData, setCafeData] = useState([]);
+  const params = {
+    category_group_code: "CE7", //카페 카테고리 코드
+    radius: 200, // 반경 설정
+    x: 127.05327728385645,
+    y: 37.54480274987832,
+  };
+  const headers = {
+    Authorization: "KakaoAK 36bc1dfae15cdd50ef0fca451fbecbbd",
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://dapi.kakao.com/v2/local/search/category.json", {
+        params,
+        headers,
+      })
+      .then((response) => {
+        // console.log(location);
+        setCafeData(response.data.documents);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(cafeData);
+    cafeData.map((el, index) => {
+      console.log(parseFloat(el.x), parseFloat(el.y));
+    });
+  }, [cafeData]);
+
+  // 마커 클릭 시 카페 상세 정보창 노출
+  const [selectedMarker, setSelectedMarker] = useState(null);
   const handleMarkerClick = (marker) => {
     setSelectedMarker(marker);
   };
   useEffect(() => {
     console.log(selectedMarker);
-    if (selectedMarker && selectedMarker.code) {
-      const location = new navermaps.LatLng(...selectedMarker.code);
+    if (selectedMarker) {
+      const location = new navermaps.LatLng(
+        parseFloat(selectedMarker.y),
+        parseFloat(selectedMarker.x)
+      );
       const infoWindow = new navermaps.InfoWindow({
-        content: `<div style="padding:5px">${selectedMarker.station}</div>`,
+        content: `
+        <div class="cafeInfoWindow">
+          <p>${selectedMarker.place_name}</p>
+          <p>${selectedMarker.address_name}</p>
+          <p>${selectedMarker.phone}</p>
+        </div>
+        `,
       });
       infoWindow.open(map, location);
     }
   }, [selectedMarker]);
-
-  const cafeData = [
-    { order: 11, station: "잠실새내", code: [37.511687, 127.086162] },
-    { order: 23, station: "종합운동장", code: [37.510997, 127.073642] },
-    { order: 1456, station: "삼성", code: [37.508844, 127.06316] },
-    { order: 71, station: "선릉", code: [37.504503, 127.049008] },
-    { order: 1341, station: "역삼", code: [37.500622, 127.036456] },
-    { order: 65, station: "강남", code: [37.497175, 127.027926] },
-    { order: 333, station: "교대", code: [37.493415, 127.01408] },
-    { order: 575, station: "방배", code: [37.481426, 126.997596] },
-    { order: 3, station: "사당", code: [37.47653, 126.981685] },
-    { order: 578, station: "신대방", code: [37.487462, 126.913149] },
-    {
-      order: 976,
-      station: "구로디지털단지",
-      code: [37.485266, 126.901401],
-    },
-    { order: 1343, station: "신도림", code: [37.508725, 126.891295] },
-    { order: 2345, station: "문래", code: [37.517933, 126.89476] },
-  ];
 
   const currentLocation = (location) => {
     // 현재 위치 지역명 구하기
@@ -81,7 +105,7 @@ function App() {
       `<div style="padding:10px;">현재 위치 : ${localAddress}</div>`
     );
     currentWindow.open(map, location);
-    console.log("Coordinates: " + location.toString());
+    console.log("현재위치: " + location.toString());
   }
 
   function onErrorGeolocation() {
@@ -128,16 +152,18 @@ function App() {
       <MapDiv style={{ width: "100%", height: "calc(100vh - 86px)" }}>
         <NaverMap
           defaultCenter={{ lat: 37.5666102, lng: 126.9783881 }} // 초기 중심 좌표
-          defaultZoom={13} // 초기 줌 레벨
+          defaultZoom={17} // 초기 줌 레벨
           disableKineticPan={false} // 관성 드래그
           ref={setMap}
         >
-          {cafeData.map((el) => (
+          {cafeData.map((el, index) => (
             <Marker
-              key={el.order}
-              position={new navermaps.LatLng(...el.code)}
+              key={index}
+              position={
+                new navermaps.LatLng(parseFloat(el.y), parseFloat(el.x))
+              }
               icon={{
-                content: `<button class="markerBox"></button>`,
+                content: `<button class="markerBox" title="${el.place_name}"></button>`,
               }}
               onClick={() => {
                 handleMarkerClick(el);
